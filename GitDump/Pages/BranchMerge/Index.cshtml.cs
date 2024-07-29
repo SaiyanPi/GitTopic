739b5ea69d6bd844c1ec6f732467d13b0e19ecb6
neerajan rai <141233559+SayianPi@users.noreply.github.com>
2024-07-29 17:21:26 +0545

    branch and merge test creating new file

---
+++ GitDump/Pages/BranchMerge/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GitDump.Pages.BranchMerge
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
