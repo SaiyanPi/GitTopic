7bad6f6316b45301ba6c3790d1b76a05b87489f0
neerajan rai <141233559+SayianPi@users.noreply.github.com>
2024-07-29 17:51:41 +0545

    fourth commit from branch hotfix

---
+++ GitDump/Pages/BranchMerge/hotfix.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GitDump.Pages.BranchMerge
+{
+    public class hotfixModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
