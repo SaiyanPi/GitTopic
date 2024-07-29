819edf70b3b07cb0b9544dbfc06e4862f4b8aaa9
neerajan rai <141233559+SayianPi@users.noreply.github.com>
2024-07-29 17:40:37 +0545

    first commit from master

---
+++ GitDump/Pages/BranchMerge/masterCommit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GitDump.Pages.BranchMerge
+{
+    public class masterCommitModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
